a3352d3655a1187f372afeb30a8653a1292f7356
Emmet <emmet.mack10@gmail.com>
2026-01-24 15:40:11 -0600

    Registered models in admin to see them in the admin UI

--- coffee_tracker/admin.py
+++ coffee_tracker/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Roaster, BeanBag, BrewMethod, Recipe, Cup
 # Register your models here.
+admin.site.register(Roaster)
+admin.site.register(BeanBag)
+admin.site.register(BrewMethod)
+admin.site.register(Recipe)
+admin.site.register(Cup)
