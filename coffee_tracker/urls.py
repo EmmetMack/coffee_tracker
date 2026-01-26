1f203c68714cac6c835e7182ddccdd863bacfe90
Emmet <emmet.mack10@gmail.com>
2026-01-25 22:20:35 -0600

    Added the bean view, template, and URL. Able to see it in the browser

--- coffee_tracker/urls.py
+++ coffee_tracker/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RoasterListView
+from .views import RoasterListView, BeanBagListView
 
 urlpatterns = [
     path('roasters/', RoasterListView.as_view(), name='roaster-list'),
+    path('beans', BeanBagListView.as_view(), name='beanbag-list'),
 ]
--- coffee_tracker/views.py
+++ coffee_tracker/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from .models import Roaster
+from .models import Roaster, BeanBag
 # Create your views here.
 
 class RoasterListView(ListView):
     model = Roaster
     template_name = 'coffee_tracker/roaster_list.html'
     context_object_name = 'roasters'
+
+class BeanBagListView(ListView):
+    model = BeanBag
+    template_name = 'coffee_tracker/bean_bag_list.html'
+    context_object_name = 'bean_bags'
 
